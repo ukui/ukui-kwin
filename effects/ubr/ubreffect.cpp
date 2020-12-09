0443a3cbfc28ec6ad33ff9345027ddb1e192e793
Yue-Lan <lanyue@ubuntukylin.com>
2020-12-09 10:23:41 +0800

    fix ubr effect doesn't work after maximize and restore.

--- effects/ubr/ubreffect.cpp
+++ effects/ubr/ubreffect.cpp
@@ -58,6 +58,9 @@ UBREffect::UBREffect(QObject *parent, const QVariantList &args)
     auto effectsManager = KWin::effects;
 
     for (auto window : effectsManager->stackingOrder()) {
+
+        //FIXME: init with maximized window
+
         bool isUKUIDecoration = XAtomHelper::getInstance()->isUKUIDecorationWindow(window);
         window->setData(IsUKUIDecoration, isUKUIDecoration);
 
@@ -83,6 +86,9 @@ UBREffect::UBREffect(QObject *parent, const QVariantList &args)
     }
 
     connect(effectsManager, &KWin::EffectsHandler::windowAdded, this, [=](KWin::EffectWindow *window){
+
+        //FIXME: init with maximized window
+
         bool isUKUIDecoration = XAtomHelper::getInstance()->isUKUIDecorationWindow(window);
         window->setData(IsUKUIDecoration, isUKUIDecoration);
 
@@ -112,6 +118,14 @@ UBREffect::UBREffect(QObject *parent, const QVariantList &args)
         window->setData(UnityBorderRadius, QVariant());
         window->setData(IsCSD, QVariant());
         window->setData(IsUKUIDecoration, QVariant());
+    });
+
+    connect(effectsManager, &KWin::EffectsHandler::windowMaximizedStateChanged, this, [=](KWin::EffectWindow *w, bool maxHorz, bool maxVert){
+        if (maxHorz || maxVert) {
+            maximizedWindows.append(w);
+        } else {
+            maximizedWindows.removeOne(w);
+        }
     });
 }
 
